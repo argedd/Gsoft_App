0e56bed3d092be50a2a3dbf0cf58b32e842cacae
argedd <ingargediaz@gmail.com>
2024-06-07 17:51:32 -0400

    graficos culminados

--- core/views/redes/estadisticas/estadisticas_view.tsx
+++ core/views/redes/estadisticas/estadisticas_view.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { BackButton, DialogComponent } from '../../../components/components';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { BackButton, DialogComponent, LoadingComponent } from '../../../components/components';
 import { RootStackParamListRoute } from '../../../navigations/routes/app_routes';
 import LayoutPrimary from '../../../components/layouts/layout_primary';
 import { BarChart } from "react-native-gifted-charts";
 import { getConsumo } from '../../../services/redes/redes_services';
 import { Estadisticas } from '../../../data/interfaces/estadisticas_interface';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Calendar } from 'react-native-calendars';
 import { percentHeight, percentWidth } from '../../../utils/dimensions/dimensions';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../utils/redux/store';
+import LinearGradient from 'react-native-linear-gradient';
+import moment from 'moment';
 
 type ConfiguracionViewNavigationProp = StackNavigationProp<RootStackParamListRoute>;
 
@@ -21,52 +22,85 @@ interface Props {
 
 const EstadisticasView: React.FC<Props> = ({ navigation }) => {
   const [data, setData] = useState<Estadisticas[]>([]);
+  const [chartData, setChartData] = useState<any[]>([]);
   const [showDialog, setShowDialog] = useState(false);
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [showLoading, setShowLoading] = useState(false);
+  const [startDate, setStartDate] = useState(moment().startOf('month').format('YYYY-MM-DD'));
+  const [endDate, setEndDate] = useState(moment(new Date()).format('YYYY-MM-DD'));
+  const [totalSubida, setTotalSubida] = useState(0);
+  const [totalBajada, setTotalBajada] = useState(0);
+  const [dateRange, setDateRange] = useState('');
   const contract = useSelector((state: RootState) => state.contractState);
 
-  // useEffect(() => {
-  //   const userConsumo = async () => {
-  //     const consumo = await getConsumo();
-  //     setData(consumo);
-  //   };
-  //   userConsumo();
-  // }, []);
+  useEffect(() => {
+    userConsumo(endDate);
+  }, []);
+
+  useEffect(() => {
+    if (startDate && endDate) {
+      userConsumo(endDate);
+    }
+  }, [startDate, endDate]);
 
   const userConsumo = async (endDay: string) => {
+    setShowLoading(true);
 
     const consumo = await getConsumo(contract.contract, startDate, endDay);
+    console.log(startDate);
+    console.log(consumo);
+    console.log(endDay);
     setData(consumo);
+
+    let totalDownload = 0;
+    let totalUpload = 0;
+
+    const chartDataArray = consumo.flatMap(item => {
+      totalDownload += item.download;
+      totalUpload += item.upload;
+
+      return [
+        {
+          value: item.download,
+          label: `${monthNames[item.date__month - 1]}/${String(item.date__year).slice(-2)}`,
+          frontColor: '#E20A17',  // Color Base
+          sideColor: '#FF4C50',   // Tonalidad Más Clara
+          topColor: '#A0000D',    // Tonalidad Más Oscura
+          labelTextStyle: { color: 'white' },
+          topLabelComponent: () => (
+            <Text style={styles.topLabelText}>{`${item.download} GB`}</Text>
+          )
+        },
+        {
+          value: item.upload,
+          label: '',
+          frontColor: '#50504F',  // Color Base
+          sideColor: '#787877',   // Tonalidad Más Clara
+          topColor: '#282828',    // Tonalidad Más Oscura
+          topLabelComponent: () => (
+            <Text style={styles.topLabelText}>{`${item.upload} GB`}</Text>
+          )
+        }
+      ];
+    });
+    setShowLoading(false);
+
+
+    setTotalSubida(totalUpload);
+    setTotalBajada(totalDownload);
+    setChartData(chartDataArray);
+
+    if (consumo.length > 0) {
+      const initialDate = consumo[0];
+      const finalDate = consumo[consumo.length - 1];
+      const range = `${monthNames[initialDate.date__month - 1]} ${initialDate.date__year} - ${monthNames[finalDate.date__month - 1]} ${finalDate.date__year}`;
+      setDateRange(range);
+    } else {
+      setDateRange('No hay Datos');
+    }
   };
 
   const monthNames = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
   const maxValue = Math.max(...data.map(item => Math.max(item.download, item.upload))) * 1.2;
-
-  const chartData = data.flatMap(item => [
-    {
-      value: item.download,
-      label: `${monthNames[item.date__month - 1]}/${String(item.date__year).slice(-2)}`,
-      frontColor: '#4ABFF4',
-      sideColor: '#23A7F3',
-      topColor: '#92e6f6',
-      labelTextStyle: { color: 'white' },
-      topLabelComponent: () => (
-        <Text style={styles.topLabelText}>{`${item.download} GB`}</Text>
-      )
-    },
-    {
-      value: item.upload,
-      label: '',
-      frontColor: '#28B2B3',
-      sideColor: '#0FAAAB',
-      topColor: '#66C9C9',
-      labelTextStyle: { color: 'white' },
-      topLabelComponent: () => (
-        <Text style={styles.topLabelText}>{`${item.upload} GB`}</Text>
-      )
-    }
-  ]);
 
   const toggleDialog = () => {
     setShowDialog(!showDialog);
@@ -86,16 +120,11 @@ const EstadisticasView: React.FC<Props> = ({ navigation }) => {
     return range;
   };
 
-
   const handleDayPress = async (day: any) => {
     if (!startDate) {
       setStartDate(day.dateString);
     } else if (!endDate) {
       await setEndDate(day.dateString);
-      console.log('====================================');
-      console.log(day.dateString);
-      console.log('====================================');
-      userConsumo(day.dateString)
       setShowDialog(false);
     } else {
       setStartDate('');
@@ -108,55 +137,81 @@ const EstadisticasView: React.FC<Props> = ({ navigation }) => {
     setEndDate('');
   };
 
-
   const renderTitle = () => (
-    <View style={{ marginVertical: 30 }}>
+    <View style={{ marginVertical: percentHeight(2) }}>
       <Text style={styles.titleText}>
-        Uso de Datos Mensual
+        Contrato Nº {contract.contract}
       </Text>
+      <View style={styles.containerItems}>
+        <Text style={styles.rangoDeFecha}>Rango de fecha de consumo</Text>
+        <View style={[styles.parent, styles.parentFlexBox]}>
+          <Text style={styles.text}>{dateRange !== '' ? dateRange : 'Seleccione Fechas'}</Text>
+          <TouchableOpacity onPress={toggleDialog} style={[styles.botonesCalendario, styles.parentFlexBox]}>
+            <Image style={styles.vectorIcon} resizeMode="cover" source={require("../../../assets/icons/facturacion/calendarios.png")} />
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <View style={styles.legendContainer}>
         <View style={styles.legendItem}>
-          <View style={[styles.legendColor, { backgroundColor: '#4ABFF4' }]} />
-          <Text style={styles.legendText}>Descarga</Text>
+          <View style={[styles.legendColor, { backgroundColor: '#E20A17' }]} />
+          <Text style={styles.legendText}>Bajada</Text>
         </View>
         <View style={styles.legendItem}>
-          <View style={[styles.legendColor, { backgroundColor: '#28B2B3' }]} />
+          <View style={[styles.legendColor, { backgroundColor: '#50504F' }]} />
           <Text style={styles.legendText}>Subida</Text>
         </View>
       </View>
-      <TouchableOpacity onPress={toggleDialog} >
-        <MaterialCommunityIcons name="calendar-clock-outline" size={24} color="#fff" />
-      </TouchableOpacity>
-
     </View>
   );
 
   const EstadisticasComponent = () => (
     <View style={styles.container}>
+      {showLoading && <LoadingComponent isLoading={showLoading} />}
+
       <BackButton title={'Uso de Datos'} />
-      <View style={styles.chartWrapper}>
-        {renderTitle()}
-        {data.length > 0 ? (<BarChart
-          data={chartData}
-          barWidth={40}
-          spacing={8}
-          xAxisThickness={1}
-          xAxisColor='white'
-          yAxisThickness={1}
-          yAxisColor='white'
-          yAxisTextStyle={{ color: 'white' }}
-          initialSpacing={20}
-          noOfSections={3}
-          showFractionalValues
-          showYAxisIndices
-          isAnimated
-          isThreeD
-          maxValue={maxValue}
-        />) : ''}
-
-
+      <View style={styles.containerItems}>
+        <View style={styles.chartWrapper}>
+          {renderTitle()}
+          {data.length > 0 ? (
+            <BarChart
+              data={chartData}
+              barWidth={percentWidth(10)}
+              spacing={percentWidth(8)}
+              xAxisThickness={0}
+              yAxisThickness={0}
+              yAxisTextStyle={{ color: 'white' }}
+              isAnimated
+              isThreeD
+              noOfSections={4}
+              maxValue={maxValue}
+              // showLine
+              lineConfig={{
+                color: '#F29C6E',
+                thickness: 3,
+                curved: true,
+                hideDataPoints: true,
+                shiftY: 2,
+                initialSpacing: 0,
+              }}
+            />
+          ) : (
+            <Text style={{ color: 'white', marginTop: percentHeight(2) }}>No hay datos disponibles para el rango de fechas seleccionado.</Text>
+          )}
+        </View>
+        <View>
+          <View style={styles.frameParent}>
+            <LinearGradient style={styles.totalParentLayout} locations={[0.04, 1]} colors={['rgba(182, 182, 180, 0.48)', 'rgba(80, 80, 79, 0.48)']} useAngle={true} angle={180}>
+              <Text style={[styles.totalSubida, styles.gbFlexBox]}>Total subida</Text>
+              <Text style={[styles.gb, styles.gbFlexBox]}>{totalSubida.toFixed(2)} GB</Text>
+            </LinearGradient>
+            <LinearGradient style={[styles.totalBajadaParent, styles.totalParentLayout]} locations={[0.04, 1]} colors={['rgba(182, 182, 180, 0.48)', 'rgba(80, 80, 79, 0.48)']} useAngle={true} angle={180}>
+              <Text style={[styles.totalSubida, styles.gbFlexBox]}>Total bajada</Text>
+              <Text style={[styles.gb, styles.gbFlexBox]}>{totalBajada.toFixed(2)} GB</Text>
+            </LinearGradient>
+          </View>
+        </View>
       </View>
-
     </View>
   );
 
@@ -186,15 +241,13 @@ const EstadisticasView: React.FC<Props> = ({ navigation }) => {
             }}
           />
           <View style={styles.botonesBotnPrincipalParent}>
-            <TouchableOpacity style={[styles.botonesBot,styles.botonesFlexBox]} onPress={clearDates}>
+            <TouchableOpacity style={[styles.botonesBot, styles.botonesFlexBox]} onPress={clearDates}>
               <Text style={[styles.iniciarSesin, styles.iniciarSesinTypo]}>Borrar Fechas</Text>
             </TouchableOpacity>
             <TouchableOpacity style={[styles.botonesBotnPrincipal, styles.botonesFlexBox]} onPress={toggleDialog}>
               <Text style={[styles.iniciarSesin, styles.iniciarSesinTypo]}>Cancelar</Text>
             </TouchableOpacity>
-
           </View>
-
         </DialogComponent>
       </View>
     </LayoutPrimary>
@@ -205,73 +258,139 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  containerItems: {
+    alignItems: 'center',
+  },
   chartWrapper: {
-    paddingBottom: 40,
-    borderRadius: 10,
-    marginHorizontal: 20,
-    marginTop: 26,
+    paddingBottom: percentHeight(4),
+    borderRadius: percentWidth(5),
+    marginTop: percentHeight(2),
+    width: percentWidth(90),
   },
   titleText: {
     color: 'white',
-    fontSize: 20,
+    fontSize: percentWidth(5),
     fontWeight: 'bold',
     textAlign: 'center',
   },
   legendContainer: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    marginTop: 24,
+    marginTop: percentHeight(2),
   },
   legendItem: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   legendColor: {
-    height: 12,
-    width: 12,
-    borderRadius: 6,
-    marginRight: 8,
+    height: percentHeight(1.5),
+    width: percentHeight(1.5),
+    borderRadius: percentHeight(0.75),
+    marginRight: percentWidth(4),
   },
   legendText: {
     color: 'lightgray',
   },
   topLabelText: {
     color: 'white',
-    fontSize: 12,
+    fontSize: percentWidth(3),
     textAlign: 'center',
   },
-
   botonesBotnPrincipalParent: {
     marginTop: percentHeight(2),
     alignSelf: "stretch",
-    alignItems: "center"
+    alignItems: "center",
   },
   iniciarSesin: {
     fontWeight: "600",
     fontFamily: "Inter-SemiBold",
-    color: "#fafafa"
+    color: "#fafafa",
   },
   botonesBotnPrincipal: {
-    backgroundColor: "#e20a17"
+    backgroundColor: "#e20a17",
   },
   botonesBot: {
     backgroundColor: "transparent",
-    borderWidth:1,
-    borderColor:"white"
+    borderWidth: percentWidth(0.3),
+    borderColor: "white",
   },
   iniciarSesinTypo: {
     textAlign: "left",
-    fontSize: 16
+    fontSize: percentWidth(4),
   },
   botonesFlexBox: {
     paddingHorizontal: percentWidth(8),
     justifyContent: "center",
     paddingVertical: percentHeight(1.5),
-    borderRadius: 8,
+    borderRadius: percentWidth(2),
     flexDirection: "row",
     alignSelf: "stretch",
     alignItems: "center",
-    marginBottom:8
+    marginBottom: percentHeight(1),
+  },
+  gbFlexBox: {
+    textAlign: "left",
+    color: "#fff",
+  },
+  totalParentLayout: {
+    backgroundColor: "transparent",
+    padding: percentWidth(3),
+    justifyContent: "center",
+    alignItems: "center",
+    height: percentHeight(8),
+    width: percentWidth(40),
+    borderRadius: percentWidth(2),
+  },
+  totalSubida: {
+    fontSize: percentWidth(4),
+    fontFamily: "Roboto-Regular",
+  },
+  gb: {
+    fontSize: percentWidth(6),
+    fontWeight: "500",
+    fontFamily: "Roboto-Medium",
+    marginTop: percentHeight(1),
+  },
+  totalBajadaParent: {
+    marginLeft: percentWidth(4),
+  },
+  frameParent: {
+    width: "100%",
+    flexDirection: "row",
+  },
+  parentFlexBox: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  text: {
+    fontSize: percentWidth(4),
+    fontWeight: "600",
+    fontFamily: "Roboto-Bold",
+    color: "#fff",
+    textAlign: "left",
+  },
+  vectorIcon: {
+    width: percentWidth(10),
+    height: percentWidth(10),
+  },
+  botonesCalendario: {
+    borderRadius: percentWidth(2),
+    backgroundColor: "#e20a17",
+    width: percentWidth(12),
+    padding: percentWidth(1.5),
+    marginLeft: percentWidth(2),
+  },
+  parent: {
+    width: percentWidth(50),
+    alignItems: "center",
+    paddingTop: percentHeight(2),
+  },
+  rangoDeFecha: {
+    fontSize: percentWidth(4),
+    fontFamily: "Roboto-Regular",
+    color: "#fff",
+    textAlign: "left",
+    paddingTop: percentHeight(2),
   },
 });
 
